52d2bba93d8239413d85c2b51029ffc14d285e98
arsanson <arsanson@mindspring.com>
2019-07-30 21:25:37 -0400

    adds more content to my layout, light dtyling, and basic logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,26 @@
 let wrongQs=5
 let rightQs=0
-const answers = []
+const answers = ["q1b1","q2b2","q3b3","q4b4","q5b1"]
 const qsAnswered = []
 
-$("#q1b1").on("click", selectAnswer)
+$(".questions").on("click", selectAnswer)
+$("#submit").on("click", scoringFunc)
 
 function selectAnswer (e){
-    /*qsAnswered.push(answer);
-    $(answer).disabled = true;*/
+    qsAnswered.push(e.target.id);
+    $("#"+e.target.id).disabled = true;
     console.log(e.target.id)
+}
+
+function scoringFunc() {
+    /*stop timer8*/
+    for (i=0; i<qsAnswered.length; i++){
+        if (answers.includes(qsAnswered[i])){
+            rightQs++;
+            wrongQs--;
+        } 
+    }
+    console.log("your score")
+    console.log("correct: " + rightQs)
+    console.log("incorrect: " + wrongQs)
 }
