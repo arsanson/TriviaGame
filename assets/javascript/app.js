3beb6bd269bc31fdd5aa753d50707e0b4670d121
arsanson <arsanson@mindspring.com>
2019-07-30 22:21:42 -0400

    adds timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,19 @@
+
+/*----Global-Variables----*/ 
+
 let wrongQs=5
 let rightQs=0
 const answers = ["q1b1","q2b2","q3b3","q4b4","q5b1"]
 const qsAnswered = []
+let time =120
+let intervalId = setInterval(count , 1000)
+
+/*-----Button-Management-----*/
 
 $(".questions").on("click", selectAnswer)
 $("#submit").on("click", scoringFunc)
+
+/*-------Functions---------*/
 
 function selectAnswer (e){
     qsAnswered.push(e.target.id);
@@ -13,7 +22,7 @@ function selectAnswer (e){
 }
 
 function scoringFunc() {
-    /*stop timer8*/
+    clearInterval(intervalId);
     for (i=0; i<qsAnswered.length; i++){
         if (answers.includes(qsAnswered[i])){
             rightQs++;
@@ -24,3 +33,30 @@ function scoringFunc() {
     console.log("correct: " + rightQs)
     console.log("incorrect: " + wrongQs)
 }
+
+function count() {
+
+    time--;
+    var converted = timeConverter(time);
+    /*console.log(converted);*/
+    $("#timer").text(converted);
+}
+
+function timeConverter(t) {
+
+    var minutes = Math.floor(t / 60);
+    var seconds = t - (minutes * 60);
+  
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+  
+    if (minutes === 0) {
+      minutes = "00";
+    }
+    else if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+  
+    return minutes + ":" + seconds;
+  }
